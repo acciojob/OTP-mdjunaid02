2ee85906021bf0d32dbde9b0def37c10d843b1a5
mdjunaid02 <156643108+mdjunaid02@users.noreply.github.com>
2026-02-12 10:27:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+const codes = document.querySelectorAll('.code');
+
+codes[0].focus();
+
+codes.forEach((code, index) => {
+
+  code.addEventListener('input', (e) => {
+    const value = e.target.value;
+
+    if (!/^[0-9]$/.test(value)) {
+      e.target.value = '';
+      return;
+    }
+
+    if (index < codes.length - 1) {
+      codes[index + 1].focus();
+    }
+  });
+
+  code.addEventListener('keydown', (e) => {
+    if (e.key === 'Backspace') {
+      if (code.value === '' && index > 0) {
+        codes[index - 1].focus();
+      }
+    }
+  });
+
+});
